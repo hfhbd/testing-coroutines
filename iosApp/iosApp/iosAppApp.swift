4c3578cb281dd03503ca6f589d66d0243debbb9c
hfhbd <hfhbd@users.noreply.github.com>
2022-03-15 13:59:35 +0100

    Add iOS app

---
+++ iosApp/iosApp/iosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosAppApp.swift
+//  iosApp
+//
+//  Created by Philip Wedemann on 15.03.22.
+//
+
+import SwiftUI
+
+@main
+struct iosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
